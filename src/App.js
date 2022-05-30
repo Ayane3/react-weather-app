5666dd0b35606f909abd46b214e9f1403a8bcb52
Ayane3 <ayanemanuel@gmail.com>
2022-05-30 15:54:23 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https://unrivaled-gumption-827cae.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Ayane Manuel
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://shimmering-brigadeiros-d5aa85.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
