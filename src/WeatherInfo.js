97efb2c06cf52805c22678ca574759fe16000cb2
Ayane3 <ayanemanuel@gmail.com>
2022-06-05 01:18:19 -0400

    Updated the weatherIcon component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,28 +4,6 @@ import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-  const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "CLOUDY",
-    "03n": "CLOUDY",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "10d": "RAIN",
-    "10n": "RAIN",
-
-    "11d": "RAIN",
-    "11n": "RAIN",
-    "13d": "SNOW",
-    "13n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG",
-  };
-
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
